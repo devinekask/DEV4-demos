00df28758427faa83bd1cee85c29b6e1027de0f8
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2022-02-03 09:22:53 +0100

    less space

--- 01-basics/jsx-expressions/src/App.js
+++ 01-basics/jsx-expressions/src/App.js
@@ -10,9 +10,9 @@ export default function App() {
   return (
     <div>
       <p>I'm James</p>
-      <p>I'm  {name}</p>
-      <p>I'm  {anonymous ? "anonymous" : name}</p>
-      <p>I'm  {getName()}</p>
+      <p>I'm {name}</p>
+      <p>I'm {anonymous ? "anonymous" : name}</p>
+      <p>I'm {getName()}</p>
       <button disabled={!active}>Click me</button>
     </div>
   );
